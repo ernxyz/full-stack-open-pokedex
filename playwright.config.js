f9a25e89f7a08cd700c2585301f006f95acb9234
ernxyz <ernej29@gmail.com>
2025-01-03 11:38:53 -0400

    8

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://127.0.0.1:3000",
+    url: "http://127.0.0.1:8080",
     reuseExistingServer: !process.env.CI,
   },
 });
