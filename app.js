f58dbadfc1ab5c0905d84f17ea7a351c201ffb27
ernxyz <ernej29@gmail.com>
2025-01-05 21:39:18 -0400

    check for health

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-app.get("/version", (req, res) => {
-  res.send("1");
-});
+app
+  .get("/version", (req, res) => {
+    res.send("1");
+  })
+  .get("/health", (req, res) => {
+    res.send("ok");
+  });
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
